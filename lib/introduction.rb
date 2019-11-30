13ea5e787b558e457765d7268228d4e17fada86d
Angelo Poole <angelo.poole@outlook.com>
2019-11-30 07:51:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,7 @@ end
 
 
 def introduction_with_language_optional(name, language)
-  puts "Hi, my name is #{name} and im learning to program in #{language}."
+  puts "Hi, my name is #{name} and I learning to program in #{language}."
 end
 
 def introduction_with_language(name, language="Ruby")
